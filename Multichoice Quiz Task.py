09f195aef4031037c3306bba56373238f95653d9
Seandeesheep <99300392+Seandeesheep@users.noreply.github.com>
2022-04-05 11:14:44 +1200

    Add files via upload
    Confirm button now jumps to the next question when the answer is correct

--- Multichoice Quiz Task.py
+++ Multichoice Quiz Task.py
@@ -7,30 +7,75 @@ from tkinter import *
 class MultiChoiceQuizGUI:
 
     def __init__(self, parent, quiz_questions):
+        self.q = quiz_questions
+        self.q_n = 0
+
         # initialising frame
-        main_frame = Frame(parent, width=400, height=400)
+        self.main_frame = Frame(parent, width=400, height=400)
+
+        # multichoice variable
+        self.choice = StringVar()
+        self.choice.set('')
 
         # initialising labels
-        question_label = Label(main_frame, text=1, padx=20, pady=5)
+        self.q_label = Label(self.main_frame, text=f"{self.q_n + 1}. {self.q.get_q()[self.q_n]}", padx=20, pady=5)
+
+        # initialising buttons
+        self.multichoice_button = []
+        for i in range(len(self.q.get_ans()[self.q_n])):
+            self.multichoice_button.append(
+                Radiobutton(self.main_frame, text=self.q.get_ans()[self.q_n][i], value=self.q.get_ans()[self.q_n][i],
+                            variable=self.choice))
+        confirm_button = Button(self.main_frame, text="Confirm", command=self.check_answer)
 
         # widget grid
-        main_frame.grid(sticky=N)
-        question_label.grid(sticky=NSEW, row=0, column=0)
+        self.main_frame.grid(sticky=N)
+        self.q_label.grid(sticky=NSEW, row=0, column=0)
+        for i in range(len(self.multichoice_button)):
+            self.multichoice_button[i].grid(row=i + 1, sticky=NSEW, padx=20, pady=5)
+        confirm_button.grid(row=2 + len(self.multichoice_button), pady=10)
+
+    def redisplay(self):
+        self.q_label.configure(text=f"{self.q_n + 1}. {self.q.get_q()[self.q_n]}", padx=20, pady=5)
+        for i in range(len(self.multichoice_button)):
+            self.multichoice_button[i].configure(text=self.q.get_ans()[self.q_n][i], value=self.q.get_ans()[self.q_n][i])
+
+    def check_answer(self):
+        if self.q_n >= len(self.q.get_q())-1:
+            print('done')
+        elif self.choice.get() == self.q.get_ans()[self.q_n][self.q.get_ans_index()[self.q_n]]:
+            self.q_n += 1
+            self.redisplay()
+
 
 
 class QuizQuestions:
 
-    def __init__(self, questions, answers):
+    def __init__(self, questions, answers, correct_answers):
         self.questions = questions
         self.answers = answers
+        self.correct_answers = correct_answers
 
+    def get_q(self):
+        return self.questions
+
+    def get_ans(self):
+        return self.answers
+
+    def get_ans_index(self):
+        return self.correct_answers
+
+
+# CONSTANTS
+QUIZ_1 = ["What is the capital of New Zealand", "What is the capital of China", "What is the capital of Cambodia"]
+ANSWERS_1 = [["Wellington", "Auckland", "Dunedin", "Queenstown"],
+             ["Shanghai", "Beijing", "Hong Kong", "Taiwan"],
+             ["Phnom Penh", "Angkor", "Samraong", "Krong Siem Reap"]]
+CORRECT_ANSWERS_1 = [0, 1, 0]
 
 # mainloop
 root = Tk()
-
-quiz = QuizQuestions()
+quiz = QuizQuestions(QUIZ_1, ANSWERS_1, CORRECT_ANSWERS_1)
 MultiChoiceQuizGUI(root, quiz)
-quiz_1 = ["What is the capital of New Zealand", "What is the capital of China", "What is the capital of Cambodia"]
-answer_1 = ["Wellington", "Beijing", "Phnom Penh"]
 
 root.mainloop()
